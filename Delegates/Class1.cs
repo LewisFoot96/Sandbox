d56cbc4cdbde17b27c5df9ebb92c99c7b2fe24eb
LFOOT <lewis_foot@next.co.uk>
2024-07-31 11:40:17 +0100

    feat: added test delegates

--- Delegates/Class1.cs
+++ Delegates/Class1.cs
@@ -2,4 +2,26 @@
 
 public class Class1
 {
+    Func<int, int, int> sumFunc = (a, b) => a + b;
+
+    private Action<string> _returnStringAction = Console.WriteLine;
+
+    public void TestTheDelegate()
+    {
+        TestDelegate(sumFunc, 1);
+
+        TestDelegate((test, test1) => test + test1, 1);
+        
+        TestActionDelegate(_returnStringAction);
+    }
+
+    private void TestDelegate(Func<int, int, int> mathsFunction, int test)
+    {
+        var result = mathsFunction(10, 10) + test;
+    }
+
+    private void TestActionDelegate(Action<string> testAction)
+    {
+        testAction("Hello");
+    }
 }
